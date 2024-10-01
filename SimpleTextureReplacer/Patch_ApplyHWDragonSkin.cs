d864cdc5fc6d7201ae3c8371bf708f7ad39cd355
Aidanamite <aid.dav.rei@gmail.com>
2024-10-01 17:58:42 +1300

    2.4.3: • Fixed a bug with the hidden world causing dragons to be stuck sitting and not appear as their vanilla hidden world skin

--- SimpleTextureReplacer/Patch_ApplyHWDragonSkin.cs
+++ SimpleTextureReplacer/Patch_ApplyHWDragonSkin.cs
@@ -26,22 +26,23 @@ namespace SimpleResourceReplacer
 
         static string GetResourcePath(string original, SanctuaryPet pet)
         {
-            foreach (var a in pet.pData.Accessories)
-                if (a != null && a.Type == RaisedPetAccType.Materials.ToString())
-                {
-                    var i = CommonInventoryData.pInstance.FindItem(pet.pData.GetAccessoryItemID(a));
-                    if (i?.Item?.AssetName != null)
+            if (pet && pet.pData?.Accessories != null)
+                foreach (var a in pet.pData.Accessories)
+                    if (a != null && a.Type == RaisedPetAccType.Materials.ToString())
                     {
-                        var k = new ResouceKey(i.Item.AssetName);
-                        if (k.bundle == Main.CustomBundleName)
+                        var i = CommonInventoryData.pInstance.FindItem(pet.pData.GetAccessoryItemID(a));
+                        if (i?.Item?.AssetName != null)
                         {
-                            k.resource = "HW" + k.resource;
-                            var s = CustomDragonEquipment.GetCustomAsset(k.resource);
-                            if (s)
-                                return k.FullResourceString;
+                            var k = new ResouceKey(i.Item.AssetName);
+                            if (k.bundle == Main.CustomBundleName)
+                            {
+                                k.resource = "HW" + k.resource;
+                                var s = CustomDragonEquipment.GetCustomAsset(k.resource);
+                                if (s)
+                                    return k.FullResourceString;
+                            }
                         }
                     }
-                }
             return original;
         }
     }
